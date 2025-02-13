f2ddb00773326dd3650e8a6c34ad99a7d0084b3f
vxxxxc <>
2025-02-13 18:35:56 +0800

    further render implementation

--- src/render.rs
+++ src/render.rs
@@ -1,11 +1,12 @@
 use crossterm::{
+    cursor::MoveTo,
     style::{Color, SetBackgroundColor},
     terminal::{Clear, ClearType},
     QueueableCommand,
 };
 
 use crate::frame::Frame;
-use std::io::Stdout;
+use std::io::{Stdout, Write};
 
 pub fn render(stdout: &mut Stdout, last_frame: &Frame, curr_frame: &Frame, force: bool) {
     if force {
@@ -13,4 +14,14 @@ pub fn render(stdout: &mut Stdout, last_frame: &Frame, curr_frame: &Frame, force
         stdout.queue(Clear(ClearType::All)).unwrap();
         stdout.queue(SetBackgroundColor(Color::Black)).unwrap();
     }
+
+    for (x, col) in curr_frame.iter().enumerate() {
+        for (y, string_location) in col.iter().enumerate() {
+            if *string_location != last_frame[x][y] || force {
+                stdout.queue(MoveTo(x as u16, y as u16)).unwrap();
+                print!("{}", *string_location);
+            }
+        }
+    }
+    stdout.flush().unwrap();
 }
