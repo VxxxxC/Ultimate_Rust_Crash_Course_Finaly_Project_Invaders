8bf68df55f1c2c6e011d1729bdf14538ee57ea7b
vxxxxc <>
2025-02-13 18:10:22 +0800

    set ClearType to clean the terminal buffer, then set the background to black color

--- src/render.rs
+++ src/render.rs
@@ -1,5 +1,6 @@
 use crossterm::{
     style::{Color, SetBackgroundColor},
+    terminal::{Clear, ClearType},
     QueueableCommand,
 };
 
@@ -9,5 +10,7 @@ use std::io::Stdout;
 pub fn render(stdout: &mut Stdout, last_frame: &Frame, curr_frame: &Frame, force: bool) {
     if force {
         stdout.queue(SetBackgroundColor(Color::Blue)).unwrap();
+        stdout.queue(Clear(ClearType::All)).unwrap();
+        stdout.queue(SetBackgroundColor(Color::Black)).unwrap();
     }
 }
